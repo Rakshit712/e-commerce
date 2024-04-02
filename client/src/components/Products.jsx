3d7967013826c322d6c27baeefca3512b03bcb51
rakshit <rakshitudp@gmail.com>
2024-04-02 10:10:51 +0530

    02/04/2024

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -18,8 +18,8 @@ function Products({ cat, filters, sort }) {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get(cat?`https://e-commerce-hkjp.onrender.com/products?category=${cat}`
-        : "https://e-commerce-hkjp.onrender.com/products");
+        const res = await axios.get(cat?`https://e-commerce-hkjp.onrender.com/api/products?category=${cat}`
+        : "https://e-commerce-hkjp.onrender.com/api/products");
         console.log(res.data)
         
         setProducts(res.data);
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://e-commerce-hkjp.onrender.com";
+const BASE_URL = "https://e-commerce-hkjp.onrender.com/api";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGFhNzRmYzRhNDI1ZmNjMTIwYzUzYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMTk5Mjc4MCwiZXhwIjoxNzEyMjUxOTgwfQ.EceKbRgTopt_sgjiaVa7NuPrfC7oc6Ntv4zvsmpVa4U"
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
